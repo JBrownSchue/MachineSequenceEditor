8dc974781bd9b0681a7860a40cb08b6248bad991
Justin Brown <JBrown@schueco.com>
2026-01-13 13:14:05 +0100

    Testing flet build

--- ui/views.py
+++ ui/views.py
@@ -64,7 +64,7 @@ class UploadView(ft.View):
         ]
 
 
-    def on_file_result(self, event: ft.FilePickerUploadEvent):
+    def on_file_result(self, event: ft.ControlEvent):
         """Processes the file selection event and updates UI status."""
         if event.files:
             selected_file = event.files[0]
